3818d4da2fbe4c81ce3e1cd337cb268cd4f4cd62
Abdelrahman <95907777+AbdelrahmanShatta@users.noreply.github.com>
2025-01-28 19:09:43 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <iostream>
+#include "BigReal.cpp"
+#include "BigReal.h"
+
+// Last Modification Date:	5/11/2023
+// Author:	Abdelrahman ashraf
+
+
+int main() {
+        BigReal a("-70.12");
+        BigReal b("-30");
+        bool d;
+        d=(a<b);
+        
+        cout<<d;
+        
+    }
